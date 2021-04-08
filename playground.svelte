0acb173bf4be87db4751dfa2058062fe66d00b47
ymlair <w_w_wcom@163.com>
2021-04-08 19:18:40 +0800

    Update playground.svelte
    add other feature

--- playground.svelte
+++ playground.svelte
@@ -1,84 +1,114 @@
-< script > let typeMap = {
-    "TINYINT": "uint8",
-    "SMALLINT": "uint16",
-    "MEDIUMINT": "uint32",
-    "INT": "int64",
-    "BIGINT": "int64",
-    "FLOAT": "int64",
-    "DOUBLE": "int64",
-    "DECIMAL": "int64",
-    "YEAR": "time.Time",
-    "TIME": "time.Time",
-    "DATE": "time.Time",
-    "DATETIME": "time.Time",
-    "TIMESTAMP": "time.Time",
-    "CHAR": "string",
-    "VARCHAR": "string",
-    "BINARY": "string",
-    "VARBINARY": "string",
-    "BLOB": "string",
-    "TEXT": "string",
-    "ENUM": "string",
-    "SET": "string"
+<script>
+	let typeMap = {
+  "TINYINT": "uint8",
+  "SMALLINT": "uint16",
+  "MEDIUMINT": "uint32",
+  "INT": "int64",
+  "BIGINT": "int64",
+  "FLOAT": "int64",
+  "DOUBLE": "int64",
+  "DECIMAL": "int64",
+  "YEAR": "time.Time",
+  "TIME": "time.Time",
+  "DATE": "time.Time",
+  "DATETIME": "time.Time",
+  "TIMESTAMP": "time.Time",
+  "CHAR": "string",
+  "VARCHAR": "string",
+  "BINARY": "string",
+  "VARBINARY": "string",
+  "BLOB": "string",
+  "TEXT": "string",
+  "ENUM": "string",
+  "SET": "string"
 };
-let md;
-let parser;
-let value = "";
-$: goStruct = getGoStruct(value)
+	let md;
+	let parser;
+	let value = "";
+	let type = 1;
+	$: goStruct = getGoStruct(value,type)
 
-const initializeRemarkable = () = >{
-    parser = window.SqlParser;
-}
-function getGoStruct(sql) {
-    let goStruct = "";
-    if (parser != undefined) try {
-        md = parser.parse(sql);
-    } catch {
-        console.log("Parse Error")
-    }
-    if (md != undefined && md.length > 0) {
-        console.log("Parse Success");
-        goStruct = "type " + changeNameFunc(md[0].name) + " struct { <br>";
-        md[0].columns.forEach((item) = >{
-            if (item.type == "column") {
-                goStruct += "" + changeNameFunc(item.name) + " " + typeMap[item.data_type.type] + " " + "`json:\"" + item.name + "\"` // " + item.comment + "<br>";
-            }
+	const initializeRemarkable = () => {
+		parser = window.SqlParser;
+	}
+	function getGoStruct(sql,type){
+		let goStruct = "";
+	 if (parser != undefined)
+		 try{
+		md = parser.parse(sql);
+			 }catch{
+			console.log("Parse Error")
+			 }
+		if (md != undefined && md.length >0){
+			console.log("Parse Success")
+			goStruct = "type "+changeNameFunc(md[0].name)+" struct { \n"
+			md[0].columns.forEach((item)=>{
+				if (item.type == "column") {
+					console.log(type)
+					switch (type){
+						case 1:
+							goStruct += ""+changeNameFunc(item.name)+" "+typeMap[item.data_type.type]+" "+"`json:\""+item.name+"\"` // "+item.comment+"\n";
+							break;
+						case 2:
+							goStruct += ""+changeNameFunc(item.name)+" "+typeMap[item.data_type.type]+" "+"`gorm:\""+item.name+"\",json:\""+item.name+"\"` // "+item.comment+"\n";
+							break;
+						case 3:
+							goStruct += item.name+",";
+							break;
+						case 4:
+							goStruct += '"'+item.name+'",';
+							break;
+					}
+				
+				}
 
-        })
-    }
-    return goStruct + "}"
-}
-function changeNameFunc(name) {
-    if (name == undefined) {
-        return
-    }
+			})
+			return goStruct + "}";
+		}
+		return "";
+	}
+	function changeNameFunc(name) {
+		if (name == undefined){ return }
     let arr = name.split('');
-    arr.map((item, index) = >{
-        arr[0] = arr[0].toUpperCase() if (item == '_') {
-            arr.splice(index, 1);
+    arr.map((item, index) => {
+			arr[0] = arr[0].toUpperCase()
+        if (item == '_') {
+            arr.splice(index,1);
             arr[index] = arr[index].toUpperCase()
         }
-    }) return arr.join('')
-} < /script>
+    })
+    return arr.join('')
+}
+</script>
 
 <svelte:head>
-  <script src="https:/ / mmzp.github.io / sql - parser / browser / sql - parser.min.js " on:load={initializeRemarkable}></script>
+	<script src="https://mmzp.github.io/sql-parser/browser/sql-parser.min.js" on:load={initializeRemarkable}></script>
 </svelte:head>
-
-<div className="MarkdownEditor ">
-  <h3>Input</h3>
-  <label htmlFor="markdown - content ">
-    Enter some markdown
-  </label>
-  <textarea
-    id="markdown - content "
+<div>Enter the table creation statement on the left</div>
+<hr />
+<input name="sql2struct" type="radio" checked on:click="{()=>{type=1}}" />convert Go struct |
+<input name="sql2struct" type="radio" on:click="{()=>{type=2}}" />with Gorm |
+<input name="sql2struct" type="radio" on:click="{()=>{type=3}}" />get table filelds |
+<input name="sql2struct" type="radio" on:click="{()=>{type=4}}" />get table fields with quote
+<hr />
+<div id="sql">
+	<textarea  rows="30" cols="80"
+    id="markdown-content"
     bind:value={value}
   />
-  <h3>Output</h3>
-  <div
-    className="content ">
-    {#if md}
-      {@html goStruct}
-    {/if}
-  </div>
-</div>"
+</div>
+<div id="struct">
+	<textarea  rows="30" cols="80"
+    id="markdown-content"
+    bind:value={goStruct}
+  />
+</div>
+
+<style>
+	#sql {
+		float:left;
+	}
+	#struct{
+		float:right;
+	}
+</style>
